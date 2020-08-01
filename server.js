25ffb51678ce7afdc2b92a75780f0b91fd734936
= <=>
2020-08-01 03:37:05 -0500

    upd

--- server.js
+++ server.js
@@ -2,11 +2,6 @@ var express = require("express"),
 http = require("http"),
 app = express();
 
-
-app.get("/",function(req,res){
-  res.sendFile("index.html");
-})
-
 const AWSport = process.env.port || 3000;
 app.use(express.static(__dirname + "/templates"));
 http.createServer(app).listen(AWSport);

0414b1bbc72c9bcaa851249b53f6a0c0adb38a9d
= <=>
2020-08-01 02:17:20 -0500

    update env

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express"),
 http = require("http"),
 app = express();
 
+const AWSport = process.env.port || 3000;
 app.use(express.static(__dirname + "/templates"));
-http.createServer(app).listen(3000);
+http.createServer(app).listen(AWSport);
 console.log("Connection to the login screen has been established")

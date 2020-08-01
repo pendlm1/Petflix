6aa6ce7693c22ce3238037067ef5752012a3ea42
= <=>
2020-08-01 03:10:42 -0500

    upd

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var express = require("express"),
 http = require("http"),
 app = express();
 
+
+app.get("/",function(req,res){
+  res.sendFile("index.html");
+})
+
 const AWSport = process.env.port || 3000;
 app.use(express.static(__dirname + "/templates"));
 http.createServer(app).listen(AWSport);
